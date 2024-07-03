33ab0618f969c008bde30a459fc552fd1825ff39
iZpasito <lushituandaur@gmail.com>
2024-07-02 22:06:32 -0400

    xd1d1

--- Ubishop/urls.py
+++ Ubishop/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
-from rest_framework.authtoken import views
-from .views import login, register, insert_product, delete, buscar_productos, ubicaciones, productos_por_tienda, productos, tiendas
+from . import views
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('insert_product/', insert_product, name='insert_product'),
-    path('delete/<int:id>/', delete, name='delete'),
-    path('buscar_productos/', buscar_productos, name='buscar_productos'),
-    path('ubicaciones/', ubicaciones, name='ubicaciones'),
-    path('productos_por_tienda/<int:tienda_id>/', productos_por_tienda, name='productos_por_tienda'),
-    path('productos/', productos, name='productos'),
-    path('tiendas/', tiendas, name='tiendas'),
+    path('ubicaciones/', views.ubicaciones, name='ubicaciones'),
+    path('tiendas/', views.tiendas, name='tiendas'),
+    path('tiendas/<int:tienda_id>/productos/', views.productos_por_tienda, name='productos_por_tienda'),
+    path('productos/', views.productos, name='productos'),
+    path('buscar/', views.buscar_productos, name='buscar_productos'),
+    path('api/login/', views.login, name='login'),
+    path('api/register/', views.register, name='register'),
 ]
